d3f7a2991e3a0e1572ffc7c8fc22ebfb88b536d6
Erwin Errol Calanuga <hortsatta@gmail.com>
2023-05-07 21:12:53 +0800

    Add spinner and icon button components

---
+++ src/components/base/base-icon-button.component.tsx
@@ -0,0 +1,54 @@
+'use client';
+
+import { memo } from 'react';
+import { IconButton } from '@material-tailwind/react';
+import { cx } from 'classix';
+
+import BaseIcon from './base-icon.component';
+import BaseSpinner from './base-spinner.component';
+
+import type { ComponentProps } from 'react';
+import type { IconName } from './base-icon.component';
+
+type Props = Omit<ComponentProps<typeof IconButton>, 'children'> & {
+  name: IconName;
+  loading?: boolean;
+};
+
+const BaseIconButton = memo(function BaseIconButton({
+  name,
+  className,
+  variant = 'filled',
+  color = 'red',
+  loading,
+  ...moreProps
+}: Props) {
+  return (
+    <IconButton
+      className={cx(
+        'relative !shadow-lg',
+        color === 'red' && '!shadow-red-500/40 hover:!shadow-red-500/60',
+        color === 'red' && variant === 'filled' && 'bg-primary',
+        variant === 'text' && '!shadow-none',
+        loading && '!cursor-not-allowed !pointer-events-none',
+        className,
+      )}
+      variant={variant}
+      color={color}
+      {...moreProps}
+    >
+      {loading ? (
+        <BaseSpinner className='w-6 h-6' />
+      ) : (
+        <BaseIcon
+          name={name}
+          className='fill-current-dark'
+          width={24}
+          height={24}
+        />
+      )}
+    </IconButton>
+  );
+});
+
+export default BaseIconButton;
---
+++ src/components/base/base-spinner.component.tsx
@@ -0,0 +1,31 @@
+import { memo } from 'react';
+import { motion } from 'framer-motion';
+import type { ComponentProps } from 'react';
+
+const BaseSpinner = memo(function BaseSpinner(props: ComponentProps<'div'>) {
+  return (
+    <div {...props}>
+      <motion.svg
+        className='animate-spin'
+        preserveAspectRatio='none'
+        xmlns='http://www.w3.org/2000/svg'
+        viewBox='0 0 32 32'
+      >
+        <g>
+          <path
+            className='opacity-40'
+            d='M16,5A11,11,0,1,1,5,16,11,11,0,0,1,16,5m0-5A16,16,0,1,0,32,16,16,16,0,0,0,16,0Z'
+            fill='white'
+          />
+          <path
+            className='opacity-80'
+            fill='white'
+            d='M16,5V0A16,16,0,0,0,4.69,27.31l3.54-3.54A11,11,0,0,1,16,5Z'
+          />
+        </g>
+      </motion.svg>
+    </div>
+  );
+});
+
+export default BaseSpinner;

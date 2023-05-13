f57d5a592e26b9e5db807a1d69ecba2e5f6e8b61
Erwin Errol Calanuga <hortsatta@gmail.com>
2023-05-13 16:57:28 +0800

    Add tag component

---
+++ src/components/base/base-tag.component.tsx
@@ -0,0 +1,21 @@
+import { memo } from 'react';
+import { cx } from 'classix';
+import type { ComponentProps } from 'react';
+
+const BaseTag = memo(function BaseTag({
+  className,
+  children,
+}: ComponentProps<'div'>) {
+  return (
+    <div
+      className={cx(
+        'px-2 pb-1 pt-1.5 bg-green-500 text-sm font-semibold leading-none rounded-full uppercase',
+        className,
+      )}
+    >
+      {children}
+    </div>
+  );
+});
+
+export default BaseTag;

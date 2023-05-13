8599bf40b1e81bf804eb640c529f7ac54ce99975
Erwin Errol Calanuga <hortsatta@gmail.com>
2023-05-13 17:00:52 +0800

    Add latest release game products hooks

---
+++ src/hooks/use-latest-released-game-products.hook.ts
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react';
+
+import { useSupabase } from '#/components/core/core-supabase-provider';
+import { getLatestReleasedGameProducts } from '#/api/game-product.api';
+
+import type { GameProduct } from '#/types/game-product.type';
+
+type Result = {
+  gameProducts: GameProduct[] | undefined;
+};
+
+const useLatestReleasedGameProducts = (): Result => {
+  const { supabase } = useSupabase();
+  const [gameProducts, setGameProducts] = useState<GameProduct[] | undefined>(
+    undefined,
+  );
+
+  useEffect(() => {
+    (async () => {
+      const carouselItems = await getLatestReleasedGameProducts(supabase);
+      setGameProducts(carouselItems);
+    })();
+  }, [supabase]);
+
+  return { gameProducts };
+};
+
+export default useLatestReleasedGameProducts;

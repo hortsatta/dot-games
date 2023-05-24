9885e42de1af3ae5d5370f4c857cdc5baf7725f9
Erwin Errol Calanuga <hortsatta@gmail.com>
2023-05-25 00:46:46 +0800

    Add get genre by slug query

--- src/api/genre.api.ts
+++ src/api/genre.api.ts
@@ -21,18 +21,65 @@ export async function getAllGenres(
   try {
     const { orderBy } = options;
 
-    const { data } = await supabase
+    const { data: genreData, error } = await supabase
       .from('genre')
       .select()
       .is('is_active', true)
       .order(orderBy.columnName, { ascending: orderBy.ascending });
 
-    return (
-      data?.map(({ custom_slug, ...moreGenre }) =>
-        camelcaseKeys({ ...moreGenre, slug: custom_slug || moreGenre.slug }),
-      ) || []
+    if (error) {
+      return [];
+    }
+
+    const genres = await Promise.all(
+      genreData.map(async ({ custom_slug, ...moreGenre }) => {
+        const { data: image } = await supabase.storage
+          .from(process.env.NEXT_PUBLIC_SUPABASE_BUCKET_NAME || '')
+          .getPublicUrl(`images/genres/genre-${moreGenre.slug}.png`);
+
+        return camelcaseKeys({
+          ...moreGenre,
+          slug: custom_slug || moreGenre.slug,
+          coverImage: image.publicUrl,
+        });
+      }),
     );
+
+    return genres;
   } catch (error) {
     return [];
+  }
+}
+
+export async function getGenreBySlug(
+  supabase: SupabaseClient<Database>,
+  slug: string,
+): Promise<Genre | null> {
+  try {
+    // Set filter and fetch main game
+    const slugFilter = `slug.eq.${slug},custom_slug.eq.${slug}`;
+    const { data: genreData, error } = await supabase
+      .from('genre')
+      .select()
+      .is('is_active', true)
+      .or(slugFilter)
+      .single();
+
+    if (error) {
+      return null;
+    }
+
+    const { custom_slug, ...moreGenre } = genreData;
+    const { data: image } = await supabase.storage
+      .from(process.env.NEXT_PUBLIC_SUPABASE_BUCKET_NAME || '')
+      .getPublicUrl(`images/genres/genre-${moreGenre.slug}.png`);
+
+    return camelcaseKeys({
+      ...moreGenre,
+      slug: custom_slug || moreGenre.slug,
+      coverImage: image.publicUrl,
+    });
+  } catch (error) {
+    return null;
   }
 }

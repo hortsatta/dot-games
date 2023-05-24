175013c59eb2d555ad9be3394cb6950ab3d139a2
Erwin Errol Calanuga <hortsatta@gmail.com>
2023-05-25 00:46:13 +0800

    Add supabse media pattern

--- next.config.js
+++ next.config.js
@@ -7,9 +7,15 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'media.rawg.io',
+        hostname: process.env.NEXT_PUBLIC_RAWG_MEDIA_BASE_URL,
         port: '',
         pathname: '/media/**',
+      },
+      {
+        protocol: 'https',
+        hostname: process.env.NEXT_PUBLIC_SUPABASE_MEDIA_BASE_URL,
+        port: '',
+        pathname: '/storage/v1/object/public/main/images/**',
       },
     ],
   },

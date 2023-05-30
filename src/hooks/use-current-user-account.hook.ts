d42cd3c17a75f7ea436ca94045017f82bcdb0218
Erwin Errol Calanuga <hortsatta@gmail.com>
2023-05-30 09:55:52 +0800

    Refactor get current user account

--- src/hooks/use-current-user-account.hook.ts
+++ src/hooks/use-current-user-account.hook.ts
@@ -1,48 +1,58 @@
-import { useEffect, useState } from 'react';
-import camelcaseKeys from 'camelcase-keys';
+import { useCallback, useEffect, useState } from 'react';
 
+import { getCurrentUserAccount } from '#/api/auth.api';
 import { useSupabase } from '#/components/core/core-supabase-provider';
 import { useBoundStore } from './use-store.hook';
 
 import type { UserAccount } from '#/types/user-account.type';
 
 type Result = {
+  initialLoading: boolean;
   currentUserAccount: UserAccount | null | undefined;
 };
 
 export const useCurrentUserAccount = (): Result => {
   const { supabase } = useSupabase();
   const currentUserId = useBoundStore((state) => state.currentUserId);
+  const refreshUserAccount = useBoundStore((state) => state.refreshCurrentUser);
+  const setRefreshCurrentUser = useBoundStore(
+    (state) => state.setRefreshCurrentUser,
+  );
   const [currentUserAccount, setCurrentUserAccount] = useState<
     UserAccount | null | undefined
   >(undefined);
 
+  const getUserAccount = useCallback(async () => {
+    const userAccount = await getCurrentUserAccount(supabase);
+    setCurrentUserAccount(userAccount);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   useEffect(() => {
     (async () => {
-      try {
-        if (currentUserId == null) {
-          setCurrentUserAccount(currentUserId);
-          return;
-        }
-
-        const { data, error } = await supabase
-          .from('user_account')
-          .select()
-          .eq('user_id', currentUserId)
-          .single();
-
-        if (!data || !!error) {
-          setCurrentUserAccount(null);
-          return;
-        }
-
-        setCurrentUserAccount(camelcaseKeys(data) as UserAccount);
-      } catch (error) {
-        setCurrentUserAccount(null);
+      if (currentUserId == null) {
+        setCurrentUserAccount(currentUserId);
+        return;
       }
+      await getUserAccount();
     })();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [currentUserId]);
 
-  return { currentUserAccount };
+  useEffect(() => {
+    (async () => {
+      if (!refreshUserAccount) {
+        return;
+      }
+
+      await getUserAccount();
+      setRefreshCurrentUser(false);
+    })();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [refreshUserAccount]);
+
+  return {
+    initialLoading: currentUserAccount === undefined,
+    currentUserAccount,
+  };
 };

8159c6c6ed02655ad89c2cbce6aa520764c15aa7
Erwin Errol Calanuga <hortsatta@gmail.com>
2023-05-30 09:55:18 +0800

    Add refresh current user property

--- src/store/core.store.ts
+++ src/store/core.store.ts
@@ -5,9 +5,11 @@ import type { WishListSlice } from './wish-list.store';
 export type CoreSlice = {
   isDarkMode: boolean;
   showLogin: boolean;
+  refreshCurrentUser: boolean;
   currentUserId?: string | null;
   setIsDarkMode: (isDarkMode: boolean) => void;
   setShowLogin: (showLogin: boolean) => void;
+  setRefreshCurrentUser: (refresh: boolean) => void;
   setCurrentUserId: (currentUserId: string | null) => void;
 };
 
@@ -19,8 +21,11 @@ export const createCoreSlice: StateCreator<
 > = (set) => ({
   isDarkMode: true,
   showLogin: false,
+  refreshCurrentUser: false,
   currentUserId: undefined,
   setIsDarkMode: (isDarkMode: boolean) => set({ isDarkMode }),
   setShowLogin: (showLogin: boolean) => set({ showLogin }),
+  setRefreshCurrentUser: (refreshCurrentUser: boolean) =>
+    set({ refreshCurrentUser }),
   setCurrentUserId: (currentUserId: string | null) => set({ currentUserId }),
 });

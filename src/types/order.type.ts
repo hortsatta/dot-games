6cfedce0b8d59bfcc43f901d25eee1b12cc5bfba
Erwin Errol Calanuga <hortsatta@gmail.com>
2023-06-13 19:34:59 +0800

    Add OrderItem and Order type

--- src/types/order.type.ts
+++ src/types/order.type.ts
@@ -1,37 +1,62 @@
-import { BaseJsonDb } from './base.type';
+import type { BaseColumns, BaseJsonDb } from './base.type';
+import type { Address } from './address.type';
+
+export type OrderItem = {
+  item: {
+    discount: number;
+    price: number;
+    finalPrice: number;
+    gameIds?: number[];
+  };
+  quantity: number;
+};
+
+export type Order = Omit<BaseColumns, 'isActive'> & {
+  userId: string;
+  piClientSecret: string;
+  shippingFee: number;
+  address: Address;
+  totalAmount: number;
+  orderItems: OrderItem[];
+  date?: string;
+  status?: number;
+};
 
 export type OrderDb = {
   Row: {
     id: number;
     created_at: string;
     user_id: string;
-    payment_intent: string | null;
+    pi_client_secret: string | null;
     date: string | null;
     shipping_fee: number | null;
     address: BaseJsonDb | null;
-    total_price: number | null;
+    total_amount: number | null;
     order_items: BaseJsonDb | null;
+    status: number | null;
   };
   Insert: {
     id?: number;
     created_at?: string;
-    user_id: string;
-    payment_intent?: string | null;
+    user_id?: string;
+    pi_client_secret?: string | null;
     date?: string | null;
     shipping_fee?: number | null;
     address?: BaseJsonDb | null;
-    total_price?: number | null;
+    total_amount?: number | null;
     order_items?: BaseJsonDb | null;
+    status?: number | null;
   };
   Update: {
     id?: number;
     created_at?: string;
     user_id?: string;
-    payment_intent?: string | null;
+    pi_client_secret?: string | null;
     date?: string | null;
     shipping_fee?: number | null;
     address?: BaseJsonDb | null;
-    total_price?: number | null;
+    total_amount?: number | null;
     order_items?: BaseJsonDb | null;
+    status?: number | null;
   };
 };
